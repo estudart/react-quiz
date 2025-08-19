3a414f3c1c595254cf42d61aa132ea58eecb4605
hdxestudart <erico.studartdf@gmail.com>
2025-08-19 11:02:00 -0300

    feat: add datecounter component to the UI

--- src/DateCounter.jsx
+++ src/DateCounter.jsx
@@ -1,7 +1,16 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
+
+
+function reducer(state, action) {
+  console.log(state, action);
+  if (action.type === 'inc') return state + 1;
+  if (action.type === 'dec') return state - 1;
+  if (action.type === 'setCount') return action.payload;
+};
 
 function DateCounter() {
-  const [count, setCount] = useState(0);
+  const [count, dispatch] = useReducer(reducer, 0);
+
   const [step, setStep] = useState(1);
 
   // This mutates the date object.
@@ -9,17 +18,15 @@ function DateCounter() {
   date.setDate(date.getDate() + count);
 
   const dec = function () {
-    // setCount((count) => count - 1);
-    setCount((count) => count - step);
+    dispatch({ type: "dec" });
   };
 
   const inc = function () {
-    // setCount((count) => count + 1);
-    setCount((count) => count + step);
+    dispatch({ type: "inc" });
   };
 
   const defineCount = function (e) {
-    setCount(Number(e.target.value));
+    dispatch({ type: "setCount", payload: Number(e.target.value) });
   };
 
   const defineStep = function (e) {
